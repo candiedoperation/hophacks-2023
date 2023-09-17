42bc76e79e597b215b233aadfd8bcb1789c2c56b
Atheesh Thirumalairajan <github-commits@atheesh.org>
2023-09-16 23:27:12 -0400

    Licenses Added

--- client/src/integrals/LicensesDashboard.jsx
+++ client/src/integrals/LicensesDashboard.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
+import { Avatar, Box, Divider, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Typography } from '@mui/material';
 import * as React from 'react';
 
 const LicensesDashboard = () => {
@@ -29,7 +29,13 @@ const LicensesDashboard = () => {
     const [licenses, setLicenses] = React.useState([]);
     React.useEffect(() => {
         setLicenses((lic) => [
-            { primary: 'ReactJS', secondary: 'react, react-dom, create-react-app' }
+            { primary: 'ReactJS', secondary: 'react, react-dom, create-react-app', link: "https://react.dev/" },
+            { primary: 'MaterialUI', secondary: 'material-ui material-icons', link: "https://mui.com/" },
+            { primary: 'Axios HTTP Client', secondary: 'axios', link: "https://axios-http.com/" },
+            { primary: 'React Router', secondary: 'react-router', link: "https://reactrouter.com" },
+            { primary: 'OpenMeteo API', secondary: 'Weather and Geodecoding APIs', link: "https://open-meteo.com" },
+            { primary: 'Leaflet React', secondary: 'Leaflet, World Map API', link: "https://leafletjs.com/" },
+            { primary: 'Google Maps and Earth Engine', secondary: 'Satellite Imagery, Regional Data', link: "https://earth.google.com/" },
         ]);
     }, []);
 
@@ -37,15 +43,15 @@ const LicensesDashboard = () => {
         <Box sx={{ display: 'flex', width: '100%', height: '100%' }}>
             <Box sx={{ marginRight: '30px' }}><pre>{license}</pre></Box>
             <Divider orientation='vertical' />
-            <Box sx={{ margin: '10px' }}>
-                <Typography variant='h4'>Open Source Licenses</Typography>
+            <Box sx={{ margin: '10px', width: '100%' }}>
+                <Typography variant='h4'>Dependencies</Typography>
                 <List>
                     {
                         licenses.map((license, key) => (
-                            <ListItem>
+                            <ListItemButton onClick={() => { window.open(license.link) }}>
                                 <ListItemAvatar sx={{ minWidth: '45px' }}><Avatar sx={{ bgcolor: 'text.primary', height: '32px', width: '32px' }}>{key + 1}</Avatar></ListItemAvatar>
                                 <ListItemText primary={license.primary} secondary={license.secondary} />
-                            </ListItem>
+                            </ListItemButton>
                         ))
                     }
                 </List>
