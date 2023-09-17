c53118ae717a5809ed1fbcd34a7a6c440b2fb5a8
Tanay Shah <makerofuniverse1134@gmail.com>
2023-09-17 03:22:12 -0400

    removed unrelated files

--- earth-engine/image.py
+++
@@ -1,7 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="tF51WFIm1hXPxzxz8KFZ")
-project = rf.workspace().project("cvusa_aerials_segmentation")
-model = project.version(3).model
-
-print(model.predict("2.jpeg").json())
